2b9f802b0a19730ec3e810a78efe4d19cb5ccd70
Ayodeji <aayo.agunbiade@gmail.com>
2026-01-03 10:33:26 +0100

    Generated customer mock data

--- Customer_mock_data/generate_customer_service_data.py
+++ Customer_mock_data/generate_customer_service_data.py
@@ -12,3 +12,33 @@ fake_countries = Faker(["de_DE", "fr_FR"])
 current_time = datetime.now()
 minutes_of_calls = random.randint(1, 40)
 
+def customer_care(entries=5):
+    
+    
+    for i in range(entries):
+        data_entry = {
+            "call_id": uuid.uuid4(), #122 bit
+            "customer_id": uuid.uuid1(), #128 bits
+            "first_name": fake_countries.first_name(),
+            "last_name" : fake_countries.last_name(),
+            "job": fake.job(),
+            "age": fake.random_int(min=20, max=85),
+            "Email": fake.email(),
+            "phone_number": fake_countries.phone_number(),
+            "country_code": fake_countries.current_country_code(),
+            "address": fake_countries.address(),
+            "agent_id": random.randint(1,10_000),
+            "resolution_status": random.choice(resolutionStatus),
+            "current_time": current_time,
+            "end_call" : current_time + timedelta(minutes=minutes_of_calls)
+        
+            
+
+            
+        }
+        
+        data.append(data_entry)
+    return pd.DataFrame(data)
+
+generic_df = customer_care(5)
+print(generic_df)
