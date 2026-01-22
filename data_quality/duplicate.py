db81f4c8080b68dc1614478c7066394657ddbe0a
mayowa akinyele <mayowaakinyele@mayowas-MBP.fritz.box>
2026-01-22 21:26:54 +0100

    Improved code

--- data_quality/duplicate.py
+++ data_quality/duplicate.py
@@ -1,8 +1,29 @@
 import pandas as pd
 import numpy as np
 
-def deduplication(data, column):
+from to_lowercase_dropna import import_file
 
-    if data.duplicated(column).any() == True:
+main_data = import_file()
+
+# def deduplication(data, column):
+
+#     if data.duplicated(column).any() == True:
+#         raise ValueError(f"There is a duplicate value in the 'customer_id' field.")
+
+    # return data
+
+
+
+duplicate_values = []
+
+def deduplicatio(data, column):
+    
+    duplicate = data[data.duplicated(column)]
+    
+    if len(duplicate) >= 1:
+        duplicate_values.append(duplicate)
         raise ValueError(f"There is a duplicate value in the 'customer_id' field")
-    return data
+        
+    return duplicate
+
+print(deduplicatio(main_data, 'Customer ID'))
