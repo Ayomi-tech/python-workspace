53cc3db2f543dd67eb4701f669702e536544acf9
Ayodeji Agunbiade <aayo.agunbiade@gmail.com>
2026-01-22 23:53:18 +0100

    In progress

--- data_quality/main.py
+++ data_quality/main.py
@@ -1,18 +1,17 @@
-import pandas as py
+from column_to_int import convert_to_int
+from duplicate import deduplicatio
 from import_data import import_file
 from to_lowercase_dropna import to_lowercase_dropna
-from duplicate import deduplicatio
-from column_to_int import convert_to_int
+
 
 def main():
-    
+
     main_data = import_file()
 
-    # new_data = to_lowercase_dropna(main_data)
+    new_data = to_lowercase_dropna(main_data)
 
-    duplicate_value = deduplicatio(main_data, 'Customer ID')
+    duplicate_value = deduplicatio(new_data, 'Customer ID')
 
-    # int_valu = convert_to_int(new_data)
+    # int_value = convert_to_int(new_data)
 
     print(duplicate_value)
-
