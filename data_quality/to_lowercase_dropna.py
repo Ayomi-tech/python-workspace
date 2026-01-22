609a2f70f149ee397cb6575527256df3d7c2b311
mayowa akinyele <mayowaakinyele@mayowas-MBP.fritz.box>
2026-01-22 21:33:18 +0100

    Testing with the main data

--- data_quality/to_lowercase_dropna.py
+++ data_quality/to_lowercase_dropna.py
@@ -1,8 +1,15 @@
 import pandas as pd
+from import_data import import_file
+
+main_data = import_file()
+
 
 def to_lowercase_dropna(data):
     
     data.columns = [col.lower().replace(" ","_") for col in data.columns]
+
     data = data.dropna() #inplace=True
     
     return data
+
+print(to_lowercase_dropna(main_data))
