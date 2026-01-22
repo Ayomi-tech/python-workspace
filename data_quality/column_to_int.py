8908782fbf445fbb519b9dd60959a6025a0669a6
Ayodeji Agunbiade <aayo.agunbiade@gmail.com>
2026-01-22 21:36:49 +0100

    Checking for Mismatched data

---
+++ data_quality/column_to_int.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from to_lowercase_dropna import import_file
+
+main_data = import_file()
+
+def convert_to_int(data, column_name): 
+    '''
+    Convert column to int and return the defined error message if there's a mismatched data
+    '''
+
+    try:
+        data[column_name] = pd.to_numeric(data[column_name])
+        print(f"The field '{column_name}' is converted successfully to: {data[column_name].dtypes}")
+        
+    except ValueError:
+        raise ValueError(f"Mismatched data type: '{column_name}' Column contains values that cannot be converted to integer.")
+
+    return data
+
+print(convert_to_int(main_data, 'PricE'))
