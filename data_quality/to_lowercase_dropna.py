97aeb26532a1ba17b8683795620bc498e3b8b351
Ayodeji Agunbiade <aayo.agunbiade@gmail.com>
2026-01-26 19:53:28 +0100

    updated function to return DataFrame

--- data_quality/to_lowercase_dropna.py
+++ data_quality/to_lowercase_dropna.py
@@ -1,13 +1,12 @@
-from import_data import import_file
+from import_data import read_file
+import pandas as pd
 
-main_data = import_file()
 
-
-def to_lowercase_dropna(data):
+def to_lowercase_dropna(data: pd.DataFrame) -> pd.DataFrame:
 
     data.columns = [col.lower().replace(" ", "_") for col in data.columns]
     data = data.dropna()
 
     return data
 
-print(to_lowercase_dropna(main_data))
+print(to_lowercase_dropna(read_file()))
