9441cea40597c60167cfcd09dd98b5192f1b3d1a
Ayodeji Agunbiade <aayo.agunbiade@gmail.com>
2026-01-28 20:21:38 +0100

    In-progress

--- data_quality/main.py
+++ data_quality/main.py
@@ -1,17 +1,17 @@
 from column_to_int import convert_to_int
-from duplicate import deduplicatio
-from import_data import import_file
+# from duplicate import deduplicatio
+from import_data import read_file
 from to_lowercase_dropna import to_lowercase_dropna
 
 
 def main():
 
-    main_data = import_file()
+    main_data = read_file()
 
     new_data = to_lowercase_dropna(main_data)
 
     duplicate_value = deduplicatio(new_data, 'Customer ID')
+    
+    mismatched_value = convert_to_int(main_data)
 
-    # int_value = convert_to_int(new_data)
-
-    print(duplicate_value)
+    print(mismatched_value)
